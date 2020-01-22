bce8bbb251ba7f87f75b4d743a3ea4b7eb137e39
Jud Taylor <jud.taylor@protonmail.com>
2020-01-22 18:53:25 -0500

    Finished, tested.

---
+++ module3-nosql-and-document-oriented-databases/DS10-U3-S2-M3-Mongo.py
@@ -0,0 +1,47 @@
+import pymongo
+import sqlite3
+
+## Settings
+password = "not-real"
+path_to_sqlite_db = './'
+## Connect to mongoDB.com
+
+
+client_request_string = "mongodb://gptix:" + password + "@cluster0-shard-00-00-dxpn7.mongodb.net:27017,cluster0-shard-00-01-dxpn7.mongodb.net:27017,cluster0-shard-00-02-dxpn7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+client = pymongo.MongoClient(client_request_string)
+db = client.test
+
+## Connect to sqlite database at path
+sl_conn  = sqlite3.connect(path_to_sqlite_db + 'rpg_db.sqlite3')
+sl_curs = sl_conn.cursor()
+
+
+## get charactercreator_character rows
+sl_char_rows_SQL = 'SELECT * FROM charactercreator_character'
+sl_char_rows = sl_curs.execute(sl_char_rows_SQL).fetchall()
+
+
+### Prepare data for insert into mongoDB
+colnames = ['character_id', ' name', ' level', ' exp', ' hp', 
+            ' strength', ' intelligence', ' dexterity', ' wisdom']
+
+
+# len(sl_char_rows) # test
+# sl_char_rows[:3]  # test
+
+# one_row = sl_char_rows[0] 
+# one_row
+
+# dictionary = dict(zip(colnames, one_row))
+# print(dictionary)
+
+
+## Create list of dictionaries, each dict a character.
+char_dicts = []
+for row in sl_char_rows:
+  char_dicts.append(dict(zip(colnames, row)))
+
+
+
+# len(char_dicts)
+db.test.insert_many(char_dicts)
